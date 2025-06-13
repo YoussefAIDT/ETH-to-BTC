d7c4cedf89b0f578733475613e9bc2b43e043716
YoussefAIDT <youssefessaaidi281@gmail.com>
2025-06-13 22:11:34 +0100

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,6 @@
-from .cnn_bilstm import (
-    build_cnn_bilstm_model, train_model, predict_future_price
-)
+"""
+Module des modèles de machine learning
+"""
+from .cnn_bilstm import build_gru_model, build_cnn_bilstm_model
 
-__all__ = [
-    'build_cnn_bilstm_model', 'train_model', 'predict_future_price'
-]
+__all__ = ['build_gru_model', 'build_cnn_bilstm_model']
