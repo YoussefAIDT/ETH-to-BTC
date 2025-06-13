f4af7f8ecff0c91f40fc0217f88ac03a86eb9187
YoussefAIDT <youssefessaaidi281@gmail.com>
2025-06-14 00:50:01 +0100

    Update __init__.py

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,6 +1,1 @@
-"""
-Module de prétraitement des features
-"""
-from .preprocessing import create_sequences, scale_data
-
-__all__ = ['create_sequences', 'scale_data']
+from .preprocessing import preprocess_data
