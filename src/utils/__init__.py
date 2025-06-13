097ce9dd1d28d8254dac1e5e9a8ab8d40e145c2b
YoussefAIDT <youssefessaaidi281@gmail.com>
2025-06-13 22:01:38 +0100

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,6 @@
-from .visualization import (
-    plot_predictions, plot_future_predictions, calculate_metrics,
-    print_metrics, plot_training_history
-)
+"""
+Utilitaires pour la visualisation et l'évaluation
+"""
+from .visualization import plot_predictions, plot_training_history
 
-__all__ = [
-    'plot_predictions', 'plot_future_predictions', 'calculate_metrics',
-    'print_metrics', 'plot_training_history'
-]
+__all__ = ['plot_predictions', 'plot_training_history']
