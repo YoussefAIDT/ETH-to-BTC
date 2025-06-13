aef96e9d8fec93ea3f91e9e469774b3e6702c23e
YoussefAIDT <youssefessaaidi281@gmail.com>
2025-06-14 00:49:32 +0100

    Update __init__.py

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,6 +1,1 @@
-"""
-Module de collecte et gestion des données
-"""
-from .collector import collect_data_crypto_compare, preprocess_data
-
-__all__ = ['collect_data_crypto_compare', 'preprocess_data']
+from .collector import collect_data_crypto_compare
